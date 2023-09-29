50ae698f2d796e0900eafad70f3deed849730b43
Radomir Drndarski <radomir.drndarski@nearform.com>
2023-09-29 13:03:36 +0200

    fix: removed svelte ref

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,30 +1,24 @@
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	extends: [
+	  'plugin:react/recommended',
+	  'plugin:@typescript-eslint/recommended',
+	  'prettier',
+	  'plugin:prettier/recommended'
+	],
 	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+	  ecmaFeatures: {
+		jsx: true
+	  }
 	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
+	settings: {
+	  react: {
+		version: 'detect'
+	  }
 	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
-};
+	rules: {
+	  'prettier/prettier': 'error',
+	  'react/prop-types': 'off'
+	}
+  }
+  
