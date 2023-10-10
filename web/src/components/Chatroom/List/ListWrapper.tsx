ea35ffd7b5153cfc95d7ef6206d67592d9cdb3c8
Radomir Drndarski <radomir.drndarski@nearform.com>
2023-10-10 16:42:48 +0200

    fix: hover for create button

--- web/src/components/Chatroom/List/ListWrapper.tsx
+++ web/src/components/Chatroom/List/ListWrapper.tsx
@@ -89,7 +89,7 @@ export const ListWrapper: FC<ChatroomListProps> = ({ rtdbRef }) => {
                     value={roomName}
                   />
                   <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-medium py-2 px-4 rounded-lg w-full mx-auto"
+                    className="bg-primary text-white text-sm font-medium py-2 px-4 rounded-lg w-full mx-auto"
                     onClick={handleCreateRoom}
                     disabled={creating}
                   >
