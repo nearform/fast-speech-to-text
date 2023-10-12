4f25d1c7d1c9da99a5d00e4e027c7d6f99205480
Daniele Cammareri <daniele.cammareri@gmail.com>
2023-10-12 18:14:20 +0200

    fix: optional chaining

--- web/src/components/Chatroom/EntryExit.tsx
+++ web/src/components/Chatroom/EntryExit.tsx
@@ -11,7 +11,7 @@ type EntryExitProps = {
 export const EntryExitEvent: FC<EntryExitProps> = ({ event, user }) => {
   const room = useRecoilValue(activeRoom)
   const language =
-    room.guest.name === user ? room.guest.language : room.host.language
+    room.guest?.name === user ? room.guest.language : room.host.language
 
   return (
     <div className="entry-exit-event text-[12px] border rounded-full bg-white px-2 py-1">
