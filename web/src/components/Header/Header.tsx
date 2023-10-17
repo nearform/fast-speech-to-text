b152a13bdd7d0eb49959b723f03d28e6685255c1
Radomir Drndarski <radomir.drndarski@nearform.com>
2023-10-17 15:15:12 +0200

    fix: logout action

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -9,15 +9,15 @@ export function Header() {
     <div className="flex flex-auto flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16 w-full mb-3">
       <Branding />
       <div className="ml-auto flex flex-1 space-x-2 sm:justify-end items-center">
-        <div className="mr-2">
+        <div className="mr-2 flex items-center">
           <a
             href="https://github.com/nearform/fast-speech-to-text"
             target="_blank"
             rel="noopener noreferrer"
           >
             <GitHubIcon />
-            {currentUser && <UserActions />}
           </a>
+          {currentUser && <UserActions />}
         </div>
       </div>
     </div>
